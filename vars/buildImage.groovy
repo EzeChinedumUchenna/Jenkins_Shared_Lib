bc1615e81a31d553f3edcf466ac737e602d1ace9
EzeChinedumUchenna <ezechinedum504@gmail.com>
2023-02-08 10:54:47 +0100

    first commit

---
+++ vars/buildImage.groovy
@@ -0,0 +1,8 @@
+def call(){
+    echo "building the docker image..."
+    withCredentials([usernamePassword(credentialsId: 'docker-ub-credentials', passwordVariable: 'PWD', usernameVariable: 'USER')]) {
+        sh 'docker build -t nedumdocker/maven-java-nana:jma-2.0 .'
+        sh "echo $PWD | docker login -u $USER --password-stdin"
+        sh 'docker push nedumdocker/maven-java-nana:jma-2.0'
+    }
+}
