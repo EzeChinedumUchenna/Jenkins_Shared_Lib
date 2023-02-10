03012e57f4e93822d1c8a2438d143ec9c970775e
Eze Chinedum <102483586+EzeChinedumUchenna@users.noreply.github.com>
2023-02-10 09:53:52 +0100

    Update buildImage.groovy

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -1,8 +1,8 @@
 #!/usr/bin/env groovy
-def call(){
+def call(imageName){
     echo "building the docker image..."
     withCredentials([usernamePassword(credentialsId: 'docker-ub-credentials', passwordVariable: 'PWD', usernameVariable: 'USER')]) {
-        sh 'docker build -t nedumdocker/maven-java-nana:jma-2.0 .'
+        sh "docker build -t ${imageName} ."
         sh "echo $PWD | docker login -u $USER --password-stdin"
         sh 'docker push nedumdocker/maven-java-nana:jma-2.0'
     }
