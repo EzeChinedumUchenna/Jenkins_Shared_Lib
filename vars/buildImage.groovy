2c2f989e3fe8e69c1898ecaca959012eaf63cce9
EzeChinedumUchenna <ezechinedum504@gmail.com>
2023-10-03 06:57:19 +0100

    adding src and resources

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -2,6 +2,7 @@
 def call(imageName){
     echo "building the docker image..."
     withCredentials([usernamePassword(credentialsId: 'docker-ub-credentials', passwordVariable: 'PWD', usernameVariable: 'USER')]) {
+        sh 'sudo chmod 777 ../../var/run/docker.sock'
         sh "docker build -t ${imageName} ."
         sh "echo $PWD | docker login -u $USER --password-stdin"
         sh "docker push ${imageName}"
