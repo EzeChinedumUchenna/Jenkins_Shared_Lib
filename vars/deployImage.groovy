481b5b71f26e759f870f4f3c3e02525aa31006af
Eze Chinedum <102483586+EzeChinedumUchenna@users.noreply.github.com>
2023-10-04 20:56:37 +0100

    Update deployImage.groovy

--- vars/deployImage.groovy
+++ vars/deployImage.groovy
@@ -1,8 +1,17 @@
+// #!/usr/bin/env groovy
+
+// def branchName = env.BRANCH_NAME
+
+// def call(String imageName){
+//     echo "deploying image in ${branchName} to ACR ...."
+//     withCredentials([usernamePassword(credentialsId: 'azure_acr_cred', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
+//         sh "echo $PASS | docker login -u $USER --password-stdin nedumacr.azurecr.io"
+//         sh "docker push $imageName"
+//     }
+// }
 #!/usr/bin/env groovy
 
-def branchName = env.BRANCH_NAME
-
-def call(String imageName){
+def call(String imageName, String branchName) {
     echo "deploying image in ${branchName} to ACR ...."
     withCredentials([usernamePassword(credentialsId: 'azure_acr_cred', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
         sh "echo $PASS | docker login -u $USER --password-stdin nedumacr.azurecr.io"
