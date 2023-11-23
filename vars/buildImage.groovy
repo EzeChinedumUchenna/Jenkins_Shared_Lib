5c40d1e4fb27eb583d66c69582c3b838045b4e65
Eze Chinedum <102483586+EzeChinedumUchenna@users.noreply.github.com>
2023-11-23 09:02:47 +0100

    Update buildImage.groovy

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -1,10 +1,4 @@
 #!/usr/bin/env groovy
-def call(imageName){
+def call(){
     echo "building the docker image..."
-    withCredentials([usernamePassword(credentialsId: 'docker-ub-credentials', passwordVariable: 'PWD', usernameVariable: 'USER')]) {
-        sh 'sudo chmod 777 ../../var/run/docker.sock'
-        sh "docker build -t ${imageName} ."
-        sh "echo $PWD | docker login -u $USER --password-stdin"
-        sh "docker push ${imageName}"
-    }
-}
+    sh "docker build -t nedumacr.azurecr.io/demo-app:jma-$IMAGE_NAME .
